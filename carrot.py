a9fe6ffbca8463bd0f10d636b739d23f691c2f3a
Maxime <maxime.veysseyre@gmail.com>
2021-01-19 09:27:58 +0100

    [UPDATE] Modifications onion, tomato & carrot

--- carrot.py
+++ carrot.py
@@ -3,11 +3,6 @@ from vegetable import Vegetable
 
 class Carrot(Vegetable):
 
-    # def __init__ (self, carrot_variety, seed_number=0):
-    #     self.variety = carrot_variety
-    #     self.seed = seed_number
-    #     print(f"Creating a(n) {carrot_variety} carrot...")
-
     def plant(self, seed_number=0):
         self.seed = seed_number
         print(f"You have planted {seed_number} carrot(s) seed(s).")
